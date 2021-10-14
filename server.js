cd235056f8b0f0d5bdc7c38e6e777e7f228140dc
Jasper Avtarovski <jay@sulfuric.studio>
2021-10-15 08:10:42 +1100

    favicon

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var app = express();
 
 // Render static files
 app.use(express.static('public'));
+app.use('/assets', express.static(path.join('public/assets')));
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
